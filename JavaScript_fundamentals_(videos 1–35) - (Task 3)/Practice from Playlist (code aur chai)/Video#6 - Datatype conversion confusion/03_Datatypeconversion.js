e8cfe8c052518d73e981574613cd56ca169d4b40
Syed Riyyan Hassan <syedriyyan9@gmail.com>
2026-06-22 20:16:10 +0500

    Start with video lecture 8 of js fundamentals.

---
+++ JavaScript_fundamentals_(videos 1–35) - (Task 3)/Practice from Playlist (code aur chai)/Video#6 - Datatype conversion confusion/03_Datatypeconversion.js
@@ -0,0 +1,12 @@
+// let score = "33"
+// let score = "33ab"
+// let score = true
+// let score = false
+// let score = "riyyan"
+let number_value = Number(score)
+// console.log(score)
+
+// the same rule applies to every other conversion as well.
+// in boolean values would either be true or false
+// in string they would take string format
+
