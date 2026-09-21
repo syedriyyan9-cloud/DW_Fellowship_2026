c04e2ca4ce7a6011e6ffb52092dad290fb1bf432
syed riyyan hassan <syedriyyan9@gmail.com>
2026-09-21 08:30:20 +0500

    Learned how to upload and handle images in express js

--- NodeJS_Practice/tutorial_projects/blogging_application/index.js
+++ NodeJS_Practice/tutorial_projects/blogging_application/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const connectToDb = require("./connectToDb");
 const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
 const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication");
@@ -15,14 +17,18 @@ connectToDb("mongodb://127.0.0.1:27017/blogify");
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use("/", userRoute);
-app.get("/", (req, res) => {
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   return res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
